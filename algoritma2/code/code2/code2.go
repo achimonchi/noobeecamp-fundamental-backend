ce4bd35b5ee9d01f99a4ecd60b8c166ebad5b16e
Reyhan <achimonchi.reyhan@gmail.com>
2022-03-10 14:28:34 +0700

    no 2 code interview

--- algoritma2/code/code2/code2.go
+++ algoritma2/code/code2/code2.go
@@ -1,5 +1,34 @@
 package code2
 
 /*
+Soal
+====================
+Dalam sebuah kantong, terdapat beberapa bola.
+Ada 3 warna bola, yaitu Merah, Kuning, dan Biru.
 
- */
+Tampilkanlah jumlah bola paling banyak
+
+Diketahu :
+Bola = Merah, Biru, Biru, Kuning, Merah
+Warna = Merah, Biru, Kuning
+
+Ditanya :
+Bola apa yang paling banyak?
+Dan berapa jumlahnya?
+
+Jawab :
+Warna merah = 2
+Warna biru = 2
+Warna kuning = 1
+
+Karena merah dan biru sama, maka outputnya adalah :
+merah : 2, biru : 2
+*/
+func GetBallsByColor(balls []string, colors []string) map[string]int {
+	// lengkapi code berikut
+	total := make(map[string]int)
+	count := make(map[string]int)
+
+	return count
+
+}
