29bceef60ac415c6e4f35b47fec45a3fec6a4c52
Reyhan <achimonchi.reyhan@gmail.com>
2022-03-10 12:51:49 +0700

    if len(candles) == 0 {

---
+++ algoritma2/code/code1/code1.go
@@ -0,0 +1,29 @@
+package code1
+
+/*
+Soal
+===============
+Pada sebuah kue, terdapat beberapa lilin
+dengan tinggi yang berbeda.
+
+Saat kamu meniup lilin tersebut, maka yang kamu tiup adalah
+lilin paling tinggi. Jadi yang akan padam adalah lilin paling tinggi.
+
+Diketahui :
+Tinggi lilin = 3,2,4,4,1
+
+Ditanya :
+Berapa lilin yang akan padam ?
+
+Jawab :
+Pada lilin tersebut, yang paling tinggi adalah 4
+Dan jumlah lilin yang mempunyai tinggi 4 adalah 2 buah
+
+Maka, lilin yang akan padam sejumlah 2 buah
+*/
+func GetCandles(candles []int) int {
+	// lengkapi code berikut ...
+	count := 0
+
+	return count
+}
